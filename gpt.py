450fd550029eaaee195440599d088820121db5d4
alenissmart <alen112.scientist@gmail.com>
2023-04-07 21:13:54 +0800

    Paramatized the program

--- gpt.py
+++ gpt.py
@@ -1,19 +1,18 @@
 import openai
 
-openai.api_key = "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"
+question = input("輸入題目敘述：")
+choices = input("輸入題目選項：")
+correct_answer = input("輸入正確選項代碼：")
+
+openai.api_key = "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"
 
 ret = openai.ChatCompletion.create(
   model="gpt-3.5-turbo",
   messages=[
         {"role": "system", "content": "請扮演一位家教老師。以繁體中文回答以下來自學生的問題，並只以題目敘述和高中生的知識針對題目各個選項進行解釋。"},
-        {"role": "user", "content": """大文看見桌上有四種生物的學名分別為：Dryas octopetala，Arnica cordifolia，Dryas bipetala和Kandelia octopetala。哪兩種生物的親緣關係最相近？
-        (A)Dryas octopetala和Dryas bipetala
-        (B)Dryas octopetala和Kandelia octopetala
-        (C)Kandelia octopetala和Arnica cordifolia
-        (D)Arnica cordifolia和Dryas bipetala。
-        以上題目為什麼正確答案是A？"""},
-
+        {"role": "user", "content": """{}{}以上題目為什麼正確答案是{}？""".format(question,choices,correct_answer)},
     ]
 )
 
+print("\n")
 print(ret.choices[0].message.content)
